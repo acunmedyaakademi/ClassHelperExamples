8a4c9fc631001a18e42a630d36fd6b6b8bfcf5a2
Orhan Ekici <orhanekici@gmail.com>
2025-04-28 16:27:29 +0300

    örnekler eklendi

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ClassHelperExamples;
+using ClassHelperExamples.Helpers;
+
+var me = new Student
+{
+    FirstName = "Orhan",
+    LastName = "Ekici",
+    BirthDate = new DateOnly(1989, 3, 17),
+    Gender = "Erkek"
+};
+
+var students = new List<Student> { me };
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Öğrenci Yönetim Sistemi\n".ToUpper());
+    var inputSelection = Helper.AskOption("Yapmak istediğin işlemi seç", ["Listele", "Ekle", "Sil", "Çıkış"]);
+
+    if (inputSelection == 1)
+    {
+        // bu methodun bazı eksikleri var. mesela öğrencileri listelerken eğer öğrenci yoksa bilgi vermeli.
+        StudentHelper.ListStudents(students);
+    } else if (inputSelection == 2)
+    {
+        // buraya sihirli kodlar yaz ve diğer else if'leri de yap
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Hoşçakalın...");
+        Thread.Sleep(1000);
+        break;
+    }
+    
+    Console.WriteLine("\nMenüye dönmek için bir tuşa basın.");
+    Console.ReadKey(true);
+}
+
+
+
